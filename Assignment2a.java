531365777818fb4c71c11b04f47fdad1e3f5b89b
pranavneravetla <pranavneravetla@gmail.com>
2016-06-14 13:18:24 +0530

    Add files via upload

---
+++ Assignment2a.java
@@ -0,0 +1,25 @@
+
+public class Assignment2a {
+
+	public static void main(String[] args) {
+		int number=23;
+		float number2=7.5f;
+		int a=Math.round(number2);
+		
+		
+		
+		
+		
+		System.out.println(number  + a );
+		
+		
+		
+		
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
